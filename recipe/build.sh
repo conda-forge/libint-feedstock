64cb08ad13d67f1cd626b643caa51b6c2a68c565
felipez <tifonzafel@gmail.com>
2020-03-27 11:07:29 +0100

    fixed typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ echo ${PREFIX}
 export CXXFLAGS="${CXXFLAGS} -fopenmp"
 
 cmake -H. -Build -DCMAKE_INSTALL_PREFIX=${PREFIX}
-cmake --build build --target install -- -j{CPU_COUNT}
+cmake --build build --target install -- -j${CPU_COUNT}
