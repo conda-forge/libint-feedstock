df33c3c0beb0f53f5b3b4fcf1e59d2e591e1359c
Lori A. Burns <lori.burns@gmail.com>
2023-02-23 17:01:26 -0500

    merge recipes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,30 @@
-#!/bin/bash
 
-set -x
-echo ${PREFIX}
-export CXXFLAGS="${CXXFLAGS} -fopenmp"
+${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} \
+  -S ${SRC_DIR} \
+  -B build \
+  -G Ninja \
+  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_CXX_COMPILER=${CXX} \
+  -D CMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -D CMAKE_INSTALL_LIBDIR=lib \
+  -D BUILD_SHARED_LIBS=ON \
+  -D Eigen3_ROOT=${PREFIX} \
+  -D ENABLE_XHOST=OFF \
+  -D LIBINT2_REQUIRE_CXX_API=ON \
+  -D LIBINT2_REQUIRE_CXX_API_COMPILED=OFF \
+  -D LIBINT2_ENABLE_FORTRAN=OFF \
+  -D LIBINT2_ENABLE_PYTHON=OFF \
+  -D BUILD_TESTING=ON \
+  -D CMAKE_PREFIX_PATH="${PREFIX}"
 
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_SHARED_LIBS=ON
-cmake --build . --target install -- -j${CPU_COUNT}
+# use `--target check install` above to run ctest tests within build phase. adds about 10m.
+
+cmake --build build --target install -j${CPU_COUNT}
+
+# generation of the source tarball included the following settings (plus ints classes, AM, deriv)
+#  -D LIBINT2_SHGAUSS_ORDERING=standard
+#  -D LIBINT2_CARTGAUSS_ORDERING=standard
+#  -D LIBINT2_SHELL_SET=standard
+#  -D ERI3_PURE_SH=OFF
+#  -D ERI2_PURE_SH=OFF
