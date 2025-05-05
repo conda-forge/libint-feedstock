41370f6c569f978b0e27dd25dae5109b9119a0da
Lori A. Burns <lori.burns@gmail.com>
2025-05-05 14:42:48 -0400

    split Fortran output (#34)
    * multiout
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.17.00.00.30
    
    * fixes
    
    * official clang-cl
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.17.06.34.45
    
    * bash
    
    * bash
    
    * why fort
    
    * bash3
    
    * main req
    
    * try rearr again
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.17.12.41.44
    
    * reposition script
    
    * try for fort
    
    * feedname
    
    * no build_prefix
    
    * link
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.17.19.15.44
    
    * back to reg names
    
    * gen req for gen scriptname
    
    * tidy up
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.18.03.09.56
    
    * back to full AM
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.18.04.33.57
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
 @ECHO ON
+set "LDFLAGS=%LDFLAGS:/link =%"
 
 cmake %CMAKE_ARGS% ^
       -G "Ninja" ^
@@ -6,8 +7,6 @@ cmake %CMAKE_ARGS% ^
       -B build ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_C_COMPILER=clang-cl ^
-      -D CMAKE_CXX_COMPILER=clang-cl ^
       -D CMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996 %CFLAGS%" ^
       -D CMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 %CXXFLAGS%" ^
       -D CMAKE_INSTALL_LIBDIR="lib" ^
@@ -19,7 +18,7 @@ cmake %CMAKE_ARGS% ^
       -D ENABLE_XHOST=OFF ^
       -D REQUIRE_CXX_API=ON ^
       -D REQUIRE_CXX_API_COMPILED=OFF ^
-      -D ENABLE_FORTRAN=OFF ^
+      -D ENABLE_FORTRAN="%ENABLE_FORTRAN%" ^
       -D ENABLE_PYTHON=OFF ^
       -D BUILD_TESTING=OFF ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,15 +2,15 @@
 
 set -x
 echo ${PREFIX}
-if [ "$(uname)" == "Linux" ]; then
+if [[ "${target_platform}" == linux-* ]]; then
   # the L build hung once at [2105/2333]. not verified, but throttling Ninja here to avert out-of-memory problems.
   export CMAKE_BUILD_PARALLEL_LEVEL=2
 fi
-if [[ "${target_platform}" != "osx-arm64" ]]; then
-  export CXXFLAGS="${CXXFLAGS} -fopenmp"
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  :
 fi
 
-${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} \
+cmake ${CMAKE_ARGS} \
   -S ${SRC_DIR} \
   -B build \
   -G Ninja \
@@ -18,7 +18,7 @@ ${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} \
   -D CMAKE_BUILD_TYPE:STRING=Release \
   -D CMAKE_VERBOSE_MAKEFILE=ON \
   -D CMAKE_CXX_COMPILER=${CXX} \
-  -D CMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -D CMAKE_CXX_FLAGS="${CXXFLAGS} -fopenmp" \
   -D CMAKE_INSTALL_LIBDIR=lib \
   -D BUILD_SHARED_LIBS=ON \
   -D Eigen3_ROOT=${PREFIX} \
